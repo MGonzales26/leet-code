766a667c312d81b44a242d8ef182f238800dd0de
Megan Gonzales <megan.campbell26@yahoo.com>
2021-09-20 13:02:29 -0600

    push work in progress

---
+++ longest_substring.rb
@@ -0,0 +1,47 @@
+# Given a string s, find the length of the longest substring without repeating characters.
+
+
+# given a string, return value is an integer
+# go through and count letters until one repeats finding the largest number
+
+#nested iteration
+
+# Example 1:
+# Input: 
+s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+def longest_substring(string)
+  longest_substring_length = 0
+  string.chars.each_with_index do |char, i|
+    string.chars.each_with_index do |letter, index|
+      if char != letter && i != index
+        longest_substring_length += 1 
+      else
+        longest_substring_length = 0
+      end
+    end
+  end
+  longest_substring_length
+end
+
+p longest_substring(s)
+# Example 2:
+# Input: 
+s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: 
+s = "pwwkew"
+
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+# Example 4:
+# Input: 
+s = ""
+# Output: 0
